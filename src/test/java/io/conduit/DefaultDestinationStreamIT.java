a975e7cb1a538568ddca90b21a08b6274168231c
Haris Osmanagic <haris@meroxa.io>
2023-11-15 12:55:34 +0100

    integration test

--- src/test/java/io/conduit/DefaultDestinationStreamIT.java
+++ src/test/java/io/conduit/DefaultDestinationStreamIT.java
@@ -8,32 +8,91 @@ import java.util.UUID;
 import com.google.protobuf.ByteString;
 import io.conduit.grpc.Change;
 import io.conduit.grpc.Data;
+import io.conduit.grpc.Destination;
 import io.conduit.grpc.Destination.Run.Request;
 import io.conduit.grpc.Operation;
 import io.conduit.grpc.Record;
 import io.grpc.stub.StreamObserver;
+import lombok.SneakyThrows;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.iceberg.CatalogProperties;
+import org.apache.iceberg.Schema;
+import org.apache.iceberg.aws.s3.S3FileIOProperties;
+import org.apache.iceberg.catalog.Namespace;
+import org.apache.iceberg.catalog.TableIdentifier;
+import org.apache.iceberg.rest.RESTCatalog;
+import org.apache.iceberg.types.Types;
 import org.apache.spark.sql.SparkSession;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.mockito.ArgumentCaptor;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 
 class DefaultDestinationStreamIT {
-    private SparkSession spark;
-    private DestinationConfig config;
+    SparkSession spark;
+    DestinationConfig config;
+    Map<String, String> catalogProps;
+    Schema schema = new Schema(
+        Types.NestedField.required(1, "level", Types.StringType.get()),
+        Types.NestedField.required(2, "event_time", Types.TimestampType.withZone()),
+        Types.NestedField.required(3, "message", Types.StringType.get()),
+        Types.NestedField.optional(4, "call_stack", Types.ListType.ofRequired(5, Types.StringType.get())),
+        Types.NestedField.required(6, "event_id", Types.StringType.get()),
+        Types.NestedField.optional(7, "integer_field", Types.IntegerType.get()),
+        Types.NestedField.optional(8, "map_field", Types.MapType.ofOptional(123, 456, Types.StringType.get(), Types.StringType.get()))
+    );
+
+    Namespace namespace = Namespace.of("webapp");
+    TableIdentifier tableId = TableIdentifier.of(namespace, "logs");
 
     @BeforeEach
+    @SneakyThrows
     void setUp() {
         config = DestinationConfig.fromMap(Map.of(
+            "catalog.name", "demo",
             "namespace", "webapp",
             "table.name", "logs",
-            "catalog.name", "demo",
             "catalog.catalog-impl", "org.apache.iceberg.rest.RESTCatalog",
             "catalog.uri", "http://localhost:8181",
             "s3.endpoint", "http://localhost:9000",
             "s3.access-key-id", "admin",
             "s3.secret-access-key", "password"
         ));
+        catalogProps = Map.of(
+            CatalogProperties.CATALOG_IMPL, "org.apache.iceberg.rest.RESTCatalog",
+            CatalogProperties.URI, "http://localhost:8181",
+            CatalogProperties.WAREHOUSE_LOCATION, "s3a://warehouse/wh",
+            CatalogProperties.FILE_IO_IMPL, "org.apache.iceberg.aws.s3.S3FileIO",
+            S3FileIOProperties.ENDPOINT, config.getS3Endpoint(),
+            S3FileIOProperties.ACCESS_KEY_ID, config.getS3AccessKeyId(),
+            S3FileIOProperties.SECRET_ACCESS_KEY, config.getS3SecretAccessKey()
+        );
 
+        spark = initSpark();
+        initTable();
+    }
+
+    @SneakyThrows
+    private void initTable() {
+        try (RESTCatalog catalog = new RESTCatalog()) {
+            Configuration conf = new Configuration();
+            catalog.setConf(conf);
+            catalog.initialize("demo", catalogProps);
+
+            if (!catalog.namespaceExists(namespace)) {
+                catalog.createNamespace(namespace);
+            }
+            if (!catalog.tableExists(tableId)) {
+                catalog.createTable(tableId, schema);
+            }
+        }
+    }
+
+    private SparkSession initSpark() {
         String catalogName = config.getCatalogName();
 
         var builder = SparkSession
@@ -49,10 +108,12 @@ class DefaultDestinationStreamIT {
             .config("spark.sql.defaultCatalog", catalogName);
 
         config.getCatalogProperties().forEach((k, v) -> builder.config("spark.sql." + k, v));
-        spark = builder.getOrCreate();
+
+        return builder.getOrCreate();
     }
 
     @Test
+    @SneakyThrows
     void testInsert() {
         String jsonString = """
                 {
@@ -65,9 +126,13 @@ class DefaultDestinationStreamIT {
                 }
             """.formatted(LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME), UUID.randomUUID().toString());
 
-        var observerMock = Mockito.mock(StreamObserver.class);
-        DefaultDestinationStream stream = new DefaultDestinationStream(observerMock, spark, config.getCatalogName() + "." + config.getNamespace() + "." + config.getTableName());
-        stream.onNext(
+        var observerMock = mock(StreamObserver.class);
+        DefaultDestinationStream underTest = new DefaultDestinationStream(
+            observerMock,
+            spark,
+            config.getCatalogName() + "." + config.getNamespace() + "." + config.getTableName()
+        );
+        underTest.onNext(
             Request.newBuilder()
                 .setRecord(Record.newBuilder()
                     .setPayload(
@@ -81,5 +146,8 @@ class DefaultDestinationStreamIT {
                     .build()
                 ).build()
         );
+        var captor = ArgumentCaptor.forClass(Destination.Run.Response.class);
+        verify(observerMock).onNext(captor.capture());
+        verify(observerMock, never()).onError(any());
     }
 }
